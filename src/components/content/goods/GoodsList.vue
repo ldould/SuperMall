90c05aa43c55edec5150e49594216aed32393eb2
ldould <1914017555@qq.com>
2020-12-30 20:57:54 +0800

    goodslist修改

--- src/components/content/goods/GoodsList.vue
+++ src/components/content/goods/GoodsList.vue
@@ -1,27 +1,29 @@
 <template>
-    <div class="goods">
-        <goods-list-item v-for="(index,item) in goods" :key="index" :goodsItem="item"/>
-    </div>
+  <div class="goods">
+    <goods-list-item
+      v-for="item in goods"
+      :key="item.index"
+      :goodsItem="item"
+    />
+  </div>
 </template>
 
 <script>
-import GoodsListItem from './GoodsListItem.vue'
+import GoodsListItem from "./GoodsListItem.vue";
 export default {
-    name:'GoodsList',
-    components:{
-        GoodsListItem
+  name: "GoodsList",
+  components: {
+    GoodsListItem,
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      },
     },
-    props:{
-        goods:{
-            type:Array,
-            default(){
-                return []
-            }
-        }
-    }
-}
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,24 +1,35 @@
 <template>
-  <div>
-      <img :src="goodsItem.show.img" alt="">
+  <div class="goods-item">
+    <img :src="goodsItem.show.img" alt="" />
+    <div class="goods-info">
+      <p>{{ goodsItem.title }}</p>
+      <span class="price">{{ goodsItem.price }}</span>
+      <span class="collect">{{ goodsItem.cfav }}</span>
+    </div>
   </div>
-
 </template>
 
 <script>
 export default {
-    name:'GoodsListItem',
-    props:{
-        goodsItem:{
-            type:Object,
-            default(){
-                return {}
-            }
-        }
-    }
-}
+  name: "GoodsListItem",
+  props: {
+    goodsItem: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+};
 </script>
 
 <style>
-
+.goods-item {
+  padding-bottom: 40px;
+  position: relative;
+}
+.goods-item img {
+  width: 100%;
+  border-radius: 5px;
+}
 </style>
