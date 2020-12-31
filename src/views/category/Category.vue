a326e2ddee40948fe92084a962c74e45d836d7ec
ldou <ldou@dcampus.com>
2020-12-31 17:59:23 +0800

    bscroll的使用

--- src/views/category/Category.vue
+++ src/views/category/Category.vue
@@ -187,7 +187,30 @@ export default {
       }
     },
     mounted(){
-    this.scroll =  new BScroll(document.querySelector('.wrapper'))
+      /**
+      *默认情况下BScroll是不可以实时的监听滚动的位置
+      *probeType  0,1都是不侦测实时的位置
+      *           2 在手指滚动的过程中侦测，手指离开后的惯性滚动过程中不侦测
+      *           3 只要是滚动，都侦测
+       */
+    this.scroll =  new BScroll(document.querySelector('.wrapper'),{
+      probeType:3,
+      click:true,
+      pullUpLoad:true
+    })
+    this.scroll.on('scroll',(position) => {
+      // console.log(position)
+    })
+    this.scroll.on('pullingUp',() => {
+      console.log('上拉加载更多')
+      //发送网络请求，请求更多页的数据
+
+      //等数据请求完成，并且将新的数据展示出来后
+      setTimeout(() => {
+        this.scroll.finishPullUp()
+      },2000)
+    })
+    
     }
   
 }
